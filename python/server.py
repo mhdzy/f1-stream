694c297547a76eced99a44e668b43423a830feb8
mhdzy <mhdzy@users.noreply.github.com>
2022-08-23 18:45:51 -0400

    misc: some small updates to python script; still have issues

--- python/server.py
+++ python/server.py
@@ -1,4 +1,3 @@
-
 import genericpath
 import os
 import socket
@@ -9,57 +8,59 @@ if len(sys.argv) == 3:
     ip = sys.argv[1]
     port = int(sys.argv[2])
 else:
-    print("Run like : python3 server.py <arg1:server ip:this system IP 192.168.0.200> <arg2:server port:20777>")
-    ip = "192.168.0.24"
+    print("Usage: python3 server.py <arg1:ip:192.168.0.200> <arg2:port:20777>")
+    ip = "192.168.0.13"
     port = 20777
-    # exit(1)
 
 if False:
-    import os
     cmd = 'ifconfig | pcre2grep -o "(?<=(inet ))([\d\.]*)(?=( netmask))"'
-    ip = os.popen(cmd).read().split('\n')[1]
+    ip = os.popen(cmd).read().split("\n")[1]
     port = 20777
 
-# Create a UDP socket
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# Bind the socket to the port
-server_address = (ip, port)
-s.bind(server_address)
+def create_udp_socket(ip, port):
+    # create a socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # enable IP re-use
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((ip, port))
+    # do not block indefinitely
+    sock.settimeout(0.5)
+    return sock
 
-tick=0
-maxpackets = 2**17 - 1
-datastream = []
 
-while True:
-    if tick > maxpackets:
-        print("reached limit of" + str(maxpackets))
-        break
-    data, address = s.recvfrom(4096)
-    print("recv (" + str(tick) +")" + str(len(data)))
-    datastream.append(data)
-    tick = tick + 1
+def close_udp_socket(sock):
+    sock.close()
 
-track = "monaco"
-subfolder = "data/" + track + "/raw"
-if not genericpath.exists(subfolder):
-    os.mkdir(subfolder)
 
-for i in range(0, len(datastream)):
-  with open(subfolder + "/packet"+str(i)+".raw", "wb") as file:
-      if i % 10 == len(datastream) - 1:
-        print(str(round(i/len(datastream), 2) * 100) + "%\n")
-      tmp = file.write(datastream[i])
+def receive_udp_data(sock, n_packets=20000):
+    counter = 0
+    datastream = []
+    print("now waiting for data...")
+    while True:
+        if counter > n_packets:
+            break
+        data, _ = sock.recvfrom(4096)
+        datastream.append(data)
+        counter = counter + 1
+        if counter % 1000:
+            print(f"({counter})" + "recv (" + str(counter) + ") " + str(len(data)))
+    return data
 
-def unique(l: list = []) -> int:
-    res = []
-    uq_list = (list(set(l)))
-    for x in uq_list:
-        res.append(x)
-    return res
 
-# breakpoint
-# import pdb; pdb.set_trace()
+def export_udp_data(data, track):
+    export_folder = "data/" + track + "/raw"
+    if not genericpath.exists(export_folder):
+        os.mkdir(export_folder)
+    for i in range(0, len(data)):
+        with open(export_folder + "/packet" + str(i) + ".raw", "wb") as file:
+            _ = file.write(data[i])
 
-# enter a local python session w/ all pdb variables
-# import code; code.interact(local=vars())
+
+maxpackets = 100000
+track = "miami"
+
+sock = create_udp_socket(ip, port)
+data = receive_udp_data(sock, maxpackets)
+close_udp_socket(sock)
+export_udp_data(data, track)
